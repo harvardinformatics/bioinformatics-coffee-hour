cc0d57b318ceed556f878185e593500499af99e5
Nathan Weeks <nweeks@g.harvard.edu>
2020-05-01 13:12:02 -0400

    Initial commit for Brian's Intro to Tidyverse workshop
    Pristine
    harvardinformatics/micro-course/master/r/Session_2_IntermediateR_1_August2019.Rmd

--- binder/install.R
+++ binder/install.R
@@ -5,3 +5,6 @@ install.packages(c("highr", "knitr", "markdown", "rmarkdown", "stringr", "yaml")
 
 # 2020-04-21 Differential Expression Analysis with Limma Voom
 BiocManager::install(c("edgeR", "statmod"))
+
+# 2020-05-05 Introduction to Tidyverse
+BiocManager::install("tidyverse")
